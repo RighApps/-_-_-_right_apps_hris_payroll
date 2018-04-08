ff38e27c400ab61ffb9b1999e4a5700ef2f4074b
lioncave <danecaracena@gmail.com>
2018-04-08 17:56:39 +0800

    Push update

---
+++ Human_Resource_Information_System/z_newForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Human_Resource_Information_System
+{
+    public partial class z_newForm : Form
+    {
+        public z_newForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
