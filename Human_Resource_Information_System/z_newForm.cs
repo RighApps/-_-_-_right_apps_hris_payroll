e57c7cf7d657dd3bc3e713d52ffbbab47ace60b6
lioncave <danecaracena@gmail.com>
2018-04-08 18:03:25 +0800

    Push update

--- Human_Resource_Information_System/z_newForm.cs
+++ Human_Resource_Information_System/z_newForm.cs
@@ -15,5 +15,10 @@ namespace Human_Resource_Information_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Code Added.");
+        }
     }
 }
